9a0c406f226a77475bcef77f91a61db420b0e11c
Castro Crea <castro.crea@gmail.com>
2024-04-04 12:36:40 +0200

    ✨ Add drag window

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <div data-tauri-drag-region className="drag_region"></div>
       <body className={inter.className}>{children}</body>
     </html>
   );
